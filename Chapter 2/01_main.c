497a5fbbdfca5b663dc81719c78af267d9c67fe4
RAHUL144 <117508363+rahulsaha1045451444@users.noreply.github.com>
2024-09-12 14:00:38 +0530

    sd

--- Chapter 2/01_main.c
+++ Chapter 2/01_main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 int main()
 {
-  int n;
-  n = 25;
-  printf("%d", n);
+  int n1; // address assign
+  // assingment operater = use to ass
+  n1 = 25;
+  //int n2 (LHS) & 35 (RHS)
+  int n2 = 35;
+  printf("%d", n1);
+  printf("%d", n2);
   return 0;
 }
---
+++ Chapter 2/02_data_types.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+  int bookId = 1001;
+  char bookName[] = "C by YCH";
+  float bookPrice = 455.55;
+  return 0;
+}
