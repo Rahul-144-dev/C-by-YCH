f95a9eaaaea430f6568a0d31f6078b18376545b0
RAHUL144 <117508363+rahulsaha1045451444@users.noreply.github.com>
2024-12-12 22:42:50 +0530

    f

--- Chapter 9/Functions/04_CallByReference.c
+++ Chapter 9/Functions/04_CallByReference.c
@@ -1,26 +1,26 @@
 /* Call By Reference */
 #include <stdio.h>
-int swap(int *x, int *y);
-int swap_s(int x, int y);
+int swap1(int *x, int *y);
+int swap2(int *x, int *y);
 int main()
 {
   int a = 7, b = 5;
-  printf("Original a = %d & b = %d\n", a, b);
-  swap(&a, &b);
-  printf("Original a = %d & b = %d\n", a, b);
+  printf("Original 1 : a = %d & b = %d\n", a, b);
+  swap1(&a, &b);
+  printf("Original 3 : a = %d & b = %d\n", a, b);
   return 0;
 }
-int swap(int *x, int *y)
+int swap1(int *x, int *y)
 {
   int temp = *x;
   *x = *y;
   *y = temp;
-  printf("Function x = %d & y = %d\n", *x, *y);
+  printf("Function 2 : x = %d & y = %d\n", *x, *y);
 }
-int swap_s(int x, int y)
+int swap2(int *x, int *y)
 {
-  x = x + y;
-  y = x - y;
-  x = x - y;
-  printf("Function x = %d & y = %d\n", x, y);
+  *x = *x + *y;
+  *y = *x - *y;
+  *x = *x - *y;
+  printf("Function 2 : x = %d & y = %d\n", *x, *y);
 }
